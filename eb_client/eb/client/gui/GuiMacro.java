3aaaf6b0d22957ff7ba50d307e31670d31342bb4
James Maddison <james@rarge.co.uk>
2012-10-14 17:28:47 +0100

    More GuiTextArea work

--- eb_client/eb/client/gui/GuiMacro.java
+++ eb_client/eb/client/gui/GuiMacro.java
@@ -1,5 +1,6 @@
 package eb.client.gui;
 
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.common.Side;
@@ -26,6 +27,7 @@ public class GuiMacro extends GuiScreen {
 	private GuiTextField macroName;
 	private GuiTextArea macroDesc;
 	private GuiButton saveButton, loadButton;
+	private GuiListItem selected;
 	
 	@Override
 	public void initGui() {
@@ -37,13 +39,14 @@ public class GuiMacro extends GuiScreen {
 		files = new GuiList(mc, this, guiLeft + 6, guiTop + 6, guiWidth - 12, guiHeight - 60);
 		files.setPadding(2);
 		files.addItem(new GuiMacroItem("Test", "This is a test"));
-		files.addItem(new GuiMacroItem("Test2", "This is a also test that is very long"));
+		files.addItem(new GuiMacroItem("Test2", "This is a also test that is very, very, very, very long"));
 		
 		macroName = new GuiTextField(fontRenderer, guiLeft + 7, guiTop + files.getHeight() + 8, guiWidth - 60, 11);
 		macroName.setFocused(false);
 		
 		macroDesc = new GuiTextArea(fontRenderer, guiLeft + 7, guiTop + files.getHeight() + 22, guiWidth - 60, 32);
 		macroDesc.setFocused(false);
+		macroDesc.setMaxLength(65);
 		
 		saveButton = new GuiButton(0, guiLeft + guiWidth - 50, guiTop + files.getHeight() + 10, 45, 20, "Save");
 		loadButton = new GuiButton(1, guiLeft + guiWidth - 50, guiTop + files.getHeight() + 32, 45, 20, "Load");
@@ -69,7 +72,8 @@ public class GuiMacro extends GuiScreen {
 	@Override
 	public void updateScreen() {		
 		GuiListItem item = files.getSelected();
-		if(item != null) {
+		if(item != null && !item.equals(selected)) {
+			selected = item;
 			GuiMacroItem macroItem = (GuiMacroItem)item;
 			
 			macroName.setText(macroItem.getName());
@@ -85,6 +89,11 @@ public class GuiMacro extends GuiScreen {
 	
 	@Override
 	protected void keyTyped(char key, int keyCode) {
+		if(keyCode == Keyboard.KEY_ESCAPE) {
+			mc.displayGuiScreen(null);
+			return;
+		}
+		
 		if(macroName.isFocused()) {
 			macroName.textboxKeyTyped(key, keyCode);
 		} else if(macroDesc.isFocused()) {
--- eb_client/eb/client/gui/GuiTextArea.java
+++ eb_client/eb/client/gui/GuiTextArea.java
@@ -1,5 +1,7 @@
 package eb.client.gui;
 
+import org.lwjgl.input.Keyboard;
+
 import net.minecraft.src.FontRenderer;
 import net.minecraft.src.Gui;
 import cpw.mods.fml.common.Side;
@@ -11,9 +13,9 @@ public class GuiTextArea extends Gui {
 	private int x, y, width, height;
 	private String text;
 	private int lines;
-	private int charsPerLine;
 	private boolean focused, enabled;
-	private int cursorCounter;
+	private int cursorCounter, cursorPosition;
+	private int maxLength;
 	
 	public GuiTextArea(FontRenderer fontRenderer, int x, int y, int width, int height) {
 		this.fontRenderer = fontRenderer;
@@ -25,76 +27,148 @@ public class GuiTextArea extends Gui {
 		this.focused = false;
 		this.enabled = true;
 		this.cursorCounter = 0;
+		this.cursorPosition = 0;
+		this.maxLength = 100;
 		
 		lines = (int)Math.floor((double)(this.height - 2) / (double)fontRenderer.FONT_HEIGHT);
-		charsPerLine = (int)Math.floor((double)(this.width - 2) / (double)fontRenderer.getCharWidth('W'));
 	}
 	
 	public void updateCursorCounter() {
 		++cursorCounter;
-		
-		if(cursorCounter > 1) { cursorCounter -= 2; }
 	}
 	
 	public void setText(String text) {
-		this.text = text;
+		if(text.length() > maxLength) {
+			this.text = text.substring(0, maxLength);
+		} else {
+			this.text = text;
+		}
 	}
 	
 	public String getText() {
 		return text;
 	}
 	
+	public void setMaxLength(int length) {
+		maxLength = length;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
 	public void keyTyped(char key, int keyCode) {
-		if(focused && enabled) {
-			System.out.println(key + " " + keyCode);
-			
+		if(focused && enabled) {			
 			switch(keyCode) {
-				case 14: break; //backspace
-				case 28: break; //enter
-				case 54: break; //shift
+				case Keyboard.KEY_LEFT:
+					moveCursor(-1);
+					break;
+				
+				case Keyboard.KEY_RIGHT:
+					moveCursor(1);
+					break;
+					
+				case Keyboard.KEY_BACK:
+					removeAtCursor();
+					break;
 			}
 		}
 	}
 	
 	public void mouseClicked(int mouseX, int mouseY, int button) {
-		focused = GuiHelper.pointInRect(mouseX, mouseY, x, y, width, height);
+		setFocused(GuiHelper.pointInRect(mouseX, mouseY, x, y, width, height));
+		
+		if(focused) {
+			cursorPosition = text.length();
+		}
 	}
 	
 	public void setFocused(boolean focused) {
 		this.focused = focused;
+		cursorCounter = 0;
 	}
 	
 	public boolean isFocused() {
 		return focused;
 	}
 	
+	public void setCursorPosition(int pos) {
+		cursorPosition = pos;
+	}
+	
 	public void draw() {
 		drawRect(x - 1, y - 1, x + width + 1, y + height + 1, -6250336);
         drawRect(x, y, x + width, y + height, -16777216);
         
-        if(text.length() < charsPerLine) {
-        	drawStringOnLine(text, 0);
-        } else {
-	        int pos = 0;
-	        int currentLine = 0;
-	        while(pos < text.length()) {
-	        	String lineText;
-	        	
-	        	if(pos + charsPerLine >= text.length()) {
-	        		lineText = text.substring(pos);
-	        	} else {
-	        		lineText = text.substring(pos, pos + charsPerLine);
-	        	}
-	        	
-	        	drawStringOnLine(lineText, currentLine);
-	        	
-	        	++currentLine;
-	        	pos += charsPerLine;
-	        }
+        int pos = 0;
+        int currentLine = 0;
+        while(pos < text.length()) {
+        	String lineText = fontRenderer.trimStringToWidth(text.substring(pos), width - 2);
+
+        	drawStringOnLine(lineText, currentLine);
+        	
+        	//if the cursor lies on this line
+        	if((pos <= cursorPosition && pos + lineText.length() > cursorPosition)
+        	   || pos + lineText.length() >= text.length()) {
+        		drawCursor(pos, currentLine);
+        	}
+
+        	++currentLine;
+        	pos += lineText.length();
+
+        	if(currentLine >= lines) {
+        		break;
+        	}
         }
 	}
 	
 	private void drawStringOnLine(String str, int line) {
     	fontRenderer.drawStringWithShadow(str, x + 1, y + line * fontRenderer.FONT_HEIGHT + 1, Integer.MAX_VALUE);
+	}
+	
+	private void drawCursor(int lineStartPos, int line) {
+		if(!focused) { return; }
+		if(cursorPosition < lineStartPos) { return; }
+		if((cursorCounter / 6) % 2 != 0) { return; }
+				
+		int cursorX = x + 1;
+		if(cursorPosition > 0) {
+			if(cursorPosition >= text.length()) {
+				cursorX += fontRenderer.getStringWidth(text.substring(lineStartPos));
+			} else {
+				cursorX += fontRenderer.getStringWidth(text.substring(lineStartPos, cursorPosition));
+			}
+		}
+		
+		int cursorY = y + line * fontRenderer.FONT_HEIGHT + 2;
+		
+		fontRenderer.drawStringWithShadow("_", cursorX, cursorY, Integer.MAX_VALUE);
+	}
+	
+	private void moveCursor(int direction) {
+		if(direction < 0) {
+			direction = -1;
+		} else {
+			direction = 1;
+		}
+		
+		cursorPosition += direction;
+		cursorPosition = Math.min(cursorPosition, text.length());
+		cursorPosition = Math.max(cursorPosition, 0);
+	}
+	
+	private void removeAtCursor() {
+		if(cursorPosition == 0) { return; }
+		
+		String temp = text;
+		text = temp.substring(0, cursorPosition - 1);
+		
+		if(cursorPosition + 1 < text.length()) {
+			text += temp.substring(cursorPosition + 1);
+		}
 	}
 }
